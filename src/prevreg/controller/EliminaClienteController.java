200fa7ffefa3155df938911cc73b9c7714092255
Paula <paulafullstack@gmail.com>
2020-07-01 19:06:15 -0500

    envía modificaciones otros controladores, por revisar editar por error al conectar la consulta, por revisar editar por error de variables

--- src/prevreg/controller/EliminaClienteController.java
+++ src/prevreg/controller/EliminaClienteController.java
@@ -1,10 +1,16 @@
 package prevreg.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import prevreg.dao.ClientesDao;
+import prevreg.model.ClientesModel;
 
 /**
  * Servlet implementation class EliminaClienteController
@@ -25,6 +31,35 @@ public class EliminaClienteController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
+		int clienteId = Integer.parseInt(request.getParameter("idClientes"));
+		
+		ClientesModel cliente = new ClientesModel();
+		cliente.setIdClientes(clienteId);
+		
+		ClientesDao clienteDao = new ClientesDao();
+		boolean eliminar = clienteDao.eliminarCliente(cliente);
+		
+		List<ClientesModel>listadoeliminar = new ArrayList<ClientesModel>();
+		
+		listadoeliminar = clienteDao.leerCliente();
+		
+		String mensaje = "";		
+		
+		if (eliminar){
+			
+			mensaje = "La empresa ha sido correctamente eliminada del registro";
+			
+		}else{
+			
+			mensaje = "Ha ocurrido un error al eliminar la empresa, dao eliminar cliente";
+		}
+		
+		request.setAttribute("cumensaje", mensaje);
+		request.setAttribute("listadoempresas", listadoeliminar);
+		request.getRequestDispatcher("LeeCliente.jsp").forward(request, response);
+		
 	}
 
 	/**
--- src/prevreg/dao/ClientesDao.java
+++ src/prevreg/dao/ClientesDao.java
@@ -109,20 +109,118 @@ public class ClientesDao implements  ClientesInterfaz {
 
 	@Override
 	public boolean actualizarUsuario(ClientesModel cliente) {
-		// TODO Auto-generated method stub
-		return false;
+
+
+		Connection con = null;
+		Statement stm = null;
+		
+		boolean actualizar = false;
+		
+		String sql = "UPDATE usuarios SET nombre = values ('"+ cliente.getIdClientes() +"','" + cliente.getNombreEmpresa() +
+							"','"+ cliente.getRepresentanteEmpresa()+"','" + cliente.getRutEmpresa() + 
+							"',' " + cliente.getDireccionEmpresa() + " ','" + cliente.getTelefonoEmpresa()+ 
+							"','" + cliente.getCorreoEmpresa() + "','" + cliente.getGiroEmpresa() + 
+							"',' TO_DATE('" + cliente.getFechaIngresoE() + "', 'yyyy/mm/dd') ','" + cliente.getTrabajadoresEnero()+ 
+							"','" + cliente.getTrabajadoresAbril() + "',' " + cliente.getTrabajadoresJulio() + 
+							"','" + cliente.getTrabajadoresOctubre() + "','0','" + cliente.getRutAdministrador()+ "')";
+				
+		try {
+			con = Conexion.getConnection();
+			stm = con.createStatement();
+			stm.execute(sql);
+			actualizar = true;
+			stm.close();
+			//con.close();
+		}catch(SQLException e) {
+			System.out.println("Error: Clase UsuarioDao, método actualizar");
+			e.printStackTrace();
+		}
+		
+		return actualizar;
 	}
 
 	@Override
 	public boolean eliminarCliente(ClientesModel cliente) {
 		// TODO Auto-generated method stub
-		return false;
+		
+		
+		
+boolean eliminar = false;
+		
+		Statement stm = null;
+		Connection con = null;
+		
+		
+		
+		String sql = "DELETE from empresa where id_emp = " + cliente.getIdClientes();
+		
+		try {
+			con = Conexion.getConnection();
+			stm = con.createStatement();
+			stm.execute(sql);
+			eliminar = true;
+			stm.close();
+			//con.close();
+		}catch(SQLException e) {
+			System.out.println("Error: Clase ClientesDao, método eliminarCliente");
+			e.printStackTrace();
+		}
+		
+		return eliminar;	
+		
+		
 	}
 
 	@Override
 	public ClientesModel obtenerCliente(int rutEmpresa) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		Statement stm = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT  FROM empresa WHERE rut_empresa =" + rutEmpresa;
+		
+		ClientesModel e = new ClientesModel();
+		
+		
+		try{
+			
+			con = Conexion.getConnection();
+			stm = con.createStatement();
+			rs = stm.executeQuery(sql);
+			
+			while (rs.next()){
+				
+				//ClientesModel e = new ClientesModel();
+				e.setIdClientes(rs.getInt(1));
+				e.setNombreEmpresa(rs.getString(2));
+				e.setRepresentanteEmpresa(rs.getString(3));
+				e.setRutEmpresa(rs.getInt(4));
+				e.setDireccionEmpresa(rs.getString(5));
+				e.setTelefonoEmpresa(rs.getString(6));
+				e.setCorreoEmpresa(rs.getString(7));
+				e.setGiroEmpresa(rs.getString(8));
+				e.setFechaIngresoE(rs.getDate(9));
+				e.setTrabajadoresEnero(rs.getInt(10));
+				e.setTrabajadoresAbril(rs.getInt(11));
+				e.setTrabajadoresJulio(rs.getInt(12));
+				e.setTrabajadoresOctubre(rs.getInt(13));
+				e.setTasaAccidentabilidad(rs.getFloat(14));
+				e.setRutAdministrador(rs.getInt(15));
+			}
+			
+			stm.close();
+			rs.close();
+			//con.close();
+			
+		}catch(SQLException excep){
+			
+			System.out.println("Error en la clase de empresas, método leer empresas");
+			excep.printStackTrace();
+			
+		}
+		
+		return e;
+		
 	}
 
 	
